d04a68684bdf805f3b30fa37b4e0b14d3db88960
Logan / Vinxey <150287794+Vinxey@users.noreply.github.com>
2025-04-01 17:11:12 -0500

    only shows skymall gui while on mining island

--- render/guis/skymallGui.js
+++ render/guis/skymallGui.js
@@ -2,11 +2,17 @@ import { registerGui } from "../../guiManager"
 import { BaseGui } from "../BaseGui"
 import constants from "../../util/constants"
 import settings from "../../settings"
+import {dwarvenChecker, hollowsChecker, mineshaftCheck} from "../../util/helperFunctions"
+
+function checkAreas()
+{
+    if(dwarvenChecker.check() || hollowsChecker.check() || mineshaftCheck.check()) return true
+    return false
+}
 const skymallGui = new BaseGui(["skymallGui"], () =>
 {
-    message = `&bCurrent Skymall:\n&a${constants.data.currentSkymall}`
-
-    return message
+    if (!checkAreas()){return}
+    return `&bCurrent Skymall:\n&a${constants.data.currentSkymall}`
 }, () => {return skymallGui.isOpen() || settings.skymallGui})
 
 registerGui(skymallGui)
