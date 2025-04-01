402dbe15ba5deec0fd3bcd298db8608d072faf61
Logan / Vinxey <150287794+Vinxey@users.noreply.github.com>
2025-03-31 20:06:03 -0500

    fixed metal detector to make it not xray

--- render/metalDetectorSolver.js
+++ render/metalDetectorSolver.js
@@ -2,6 +2,8 @@
 import settings from "../settings"
 import constants from "../util/constants"
 import { drawCoolWaypoint, trace } from "../util/renderUtil"
+import constants from "../util/constants"
+const PREFIX = constants.PREFIX
 
 let chestCoords = JSON.parse(FileLib.read("Coleweight", "data/treasureChestCoords.json"))
 let lastLoc = [0, 0, 0]
@@ -40,8 +42,17 @@ register("actionBar", (dist) => {
     if (!settings.metalDetectorSolver)
         return
     let distance = parseFloat(dist)
-    if (!baseCoordinates) findBaseCoordinates();
-
+    if (!baseCoordinates) {
+            Client.showTitle("&ccenter of MoD not set","&ago to center of the crystal on top to set :D",0,40,0)
+            let x = ~~(Player.getX());
+            let y = ~~(Player.getY());
+            let z = ~~(Player.getZ());
+        if (World.getBlockAt(x+1,y-14,z+1).getType().getID() == 169 && World.getBlockAt(x-1,y-14,z-1).getType().getID() == 169){
+            baseCoordinates = [x+5,y-13,z+5]
+            print(baseCoordinates)
+        }
+        return;
+}
     if (lastLoc[0] !== Player.getX() || lastLoc[1] !== Player.getY() || lastLoc[2] !== Player.getZ()) {
         lastLoc = [Player.getX(), Player.getY(), Player.getZ()]
         playedPling = false;
@@ -52,7 +63,7 @@ register("actionBar", (dist) => {
 
     chestCoords.forEach((coordinates) => {
         let currentDistance = Math.hypot(Player.getX() - (baseCoordinates[0] - coordinates[0]), Player.getY() - (baseCoordinates[1] - coordinates[1] + 1), Player.getZ() - (baseCoordinates[2] - coordinates[2]))
-
+        //print(`${Math.hypot(Player.getX())}-(${Math.hypot(baseCoordinates[0])}-${Math.hypot(coordinates[0])}),-${Math.hypot(Player.getY())}-${Math.hypot(baseCoordinates[1])}-${Math.hypot(coordinates[1] + 1)})-${Math.hypot(Player.getZ())}-${Math.hypot(baseCoordinates[2])}-${Math.hypot(coordinates[2])})`)
         if (Math.round(currentDistance * 10) / 10 === distance) {
 
             if ([baseCoordinates[0] - coordinates[0], baseCoordinates[1] - coordinates[1], baseCoordinates[2] - coordinates[2]].join(",") === ignoreLocation) {
@@ -64,62 +75,13 @@ register("actionBar", (dist) => {
             {
                 World.playSound("note.pling", 100, 2)
                 playedPling = true;
+                
             }
 
             predictedChestLocations.push([baseCoordinates[0] - coordinates[0], baseCoordinates[1] - coordinates[1], baseCoordinates[2] - coordinates[2]])
         }
     });
 }).setCriteria('TREASURE: ${rest}').setParameter('contains');
-
-register("chat", (a) => {
-    if (predictedChestLocations[0]) ignoreLocation = predictedChestLocations[0].join(",")
-    predictedChestLocations = []
-}).setChatCriteria("&r&aYou found${*}with your &r&cMetal Detector&r&a!&r")
-
-
-function findBaseCoordinates()
-{
-    if (Date.now() - lastSearchedForBase < 15000) return;
-    let x = ~~Player.getX();
-    let y = ~~Player.getY();
-    let z = ~~Player.getZ();
-    for (let i = x - 50; i < x + 50; i++) {
-        for (let j = y + 30; j >= y - 30; j--) {
-            for (let k = z - 50; k < z + 50; k++) {
-                if (World.getBlockAt(i, j, k).getType().getID() === 156 && World.getBlockAt(i, j + 13, k).getType().getID() === 166) {
-                    baseCoordinates = getBaseCoordinates(i, j + 13, k);
-                    return;
-                }
-            }
-        }
-    }
-    lastSearchedForBase = Date.now();
-}
-
-function getBaseCoordinates(x, y, z) {
-    let loop = true;
-    let posX = x;
-    let posY = y;
-    let posZ = z;
-    if (World.getBlockAt(x, y, z).getType().getID() !== 166) return [x, y, z];
-    while (loop) {
-        loop = false;
-        if (World.getBlockAt(posX + 1, posY, posZ).getType().getID() == 166) {
-            posX++;
-            loop = true;
-        }
-        if (World.getBlockAt(posX, posY - 1, posZ).getType().getID() == 166) {
-            posY--;
-            loop = true;
-        }
-        if (World.getBlockAt(posX, posY, posZ + 1).getType().getID() == 166) {
-            posZ++;
-            loop = true;
-        }
-    }
-    return [posX, posY, posZ];
-}
-
 
 register("renderWorld", () => {
     predictedChestLocations.forEach(loc => {
