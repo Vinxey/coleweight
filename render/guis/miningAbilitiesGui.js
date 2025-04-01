9c9aa91727490372deaf7fd53ce495f098f9b59c
Logan / Vinxey <150287794+Vinxey@users.noreply.github.com>
2025-04-01 02:57:15 -0500

    Update miningAbilitiesGui.js

--- render/guis/miningAbilitiesGui.js
+++ render/guis/miningAbilitiesGui.js
@@ -2,28 +2,24 @@ import { registerGui } from "../../guiManager"
 import settings from "../../settings"
 import { capitalizeFirst, dwarvenChecker, endChecker, hollowsChecker, isPlayerHoldingDrill, Title } from "../../util/helperFunctions"
 import { BaseGui } from "../BaseGui"
+import constants from "../../util/constants"
 
 //initializes ability from data  
 activeAbilities = undefined
 addAbility(constants.data.currentAbility, 0)
 
 
-//initializes ability from data  
-activeAbilities = undefined
-addAbility(constants.data.currentAbility, 0) 
 const miningAbilitiesGui = new BaseGui(["abilityGui", "miningabilities"], () => {
-    if(!checkAreas())
+    if(!checkAreas() || constants.data.currentAbility == "") // had to add currentAbility check otherwise on first time timer would just say 0 forever and give errors etc
+           
         return
-    let leftValues = [],
-     rightValues = []
-
-    activeAbilities.forEach(ability => {
-        if(settings.miningAbilitiesSelectedIndicator && ability.name == selectedAbility)
-            leftValues.push(`&e${ability.name} CD`)
-        else
-            leftValues.push(`${ability.name} CD`)
-        rightValues.push(ability.timer + "s")
-    })
+        
+        let leftValues = [],
+        rightValues = []
+    
+        leftValues.push(`&e${activeAbilities.name} CD`)
+        rightValues.push(activeAbilities.timer + "s")
+    
 
     if(miningAbilitiesGui.isOpen() && leftValues.length < 1)
     {
@@ -38,9 +34,7 @@ const miningAbilitiesGui = new BaseGui(["abilityGui", "miningabilities"], () => 
 
     return message
 }, () => { return miningAbilitiesGui.isOpen() || settings.miningAbilitiesGui})
-let activeAbilities = [],
- selectedAbility
-
+ 
 function checkAreas()
 {
     if(dwarvenChecker.check() || hollowsChecker.check() || endChecker.check()) return true
@@ -50,14 +44,15 @@ function checkAreas()
 registerGui(miningAbilitiesGui)
 
 register("step", () => {
-    activeAbilities.forEach(ability => {
-        if(ability.timer > 0)
-            ability.timer -= 1
-        else if (ability.title.drawState == 0 && settings.miningAbilities)
-            ability.title.draw()
-    })
+    if (constants.data.currentAbility != ""){// had to add currentAbility check otherwise on first time timer would just say 0 forever and give errors etc
+        if(activeAbilities.timer > 0)
+            activeAbilities.timer -= 1
+        else if (activeAbilities.title.drawState == 0 && settings.miningAbilities && constants.data.miningAbilities != "z")
+            activeAbilities.title.draw()
+    }
+    
 }).setDelay(1)
-
+ 
 
 //gets skymall and saves it to data could be used for other things also but mainly just doing for 20% cooldown reduction thing
 
@@ -75,46 +70,33 @@ register("chat", (abilityName) =>{
     constants.data.currentAbility = abilityName
     constants.data.save()
     addAbility(abilityName)
-}).setCriteria(/&r&aYou used your &r&6(.+) &r&aPickaxe Ability!&r/g)
+}).setCriteria(/&r&aYou selected &r&e([a-zA-Z ]+) &r&aas your Pickaxe Ability. This ability will apply to all of your pickaxes!&r/g)
 
+//gets ability name through "{ability} is now available" message
+register("chat", (abilityName) => {
+    constants.data.currentAbility = abilityName
+    constants.data.save()
+}).setCriteria(/&r&a&r&6([a-zA-Z ]+) &r&ais now available!&r/g)
 
-register("worldLoad", () => {
-    activeAbilities.forEach(ability => {
-        ability.timer = ability.maxTimer/2
-    })
-})
-
-
-register("chat", (abilityName, event) => {
-    selectedAbility = capitalizeFirst(abilityName)
-}).setCriteria(/&r&aYou selected &r&e(.+) &r&aas your Pickaxe Ability. This ability will apply to all of your pickaxes!&r/g)
-
-
-register("chat", (cdSeconds, event) => {
-    if(selectedAbility == undefined || !isPlayerHoldingDrill()) return
-    addAbility(selectedAbility, cdSeconds)
-}).setCriteria(/&r&cThis ability is on cooldown for (.+)s.&r/g)
-
+//gets cooldown from chat cause sometimes its more accurate
+register("chat", (cooldown) =>{
+    if (constants.data.currentAbility != ""){
+    addAbility(constants.data.currentAbility, cooldown)
+}
+}).setCriteria(/&r&cYour pickaxe ability is on cooldown for ([0-9]+)s.&r/g)
 
 function addAbility(abilityName, timer = 0)
 {
     let found = false
     let maxTimer
 
-    switch(capitalizeFirst(abilityName))
-    {
-        case "Pickobulus":
-            maxTimer = 110
-            break
-        case "Vein seeker":
-            maxTimer = 60
-            break
-        case "Gemstone Infusion":
-        case "Hazardous Miner":
-            maxTimer = 140
-            break
-        default:
-            maxTimer = 120
+    //Gives Ability base cooldown
+    //pickobulus is a little innacurate cause for some reason its the only ability where the cooldown is effected by blegg so this just assumes no  blegg
+    if (constants.data.currentAbility == "Pickobulus"){
+        maxTimer = 50
+    }
+    else {
+        maxTimer = 120
     }
 
     //checks for active pet for bal
@@ -130,6 +112,7 @@ function addAbility(abilityName, timer = 0)
     } else{
         Skymall = false
     }
+    //print(timer)
     //if the timer has finished set the new timer based on buffs
     if (timer <= 0) {
         let multiplier = 1
@@ -158,18 +141,22 @@ function addAbility(abilityName, timer = 0)
     //if ability is swapped change the name of the ability on the gui
     if (activeAbilities != undefined && abilityName != activeAbilities.name){
             found = true
-            drawTimestamp = undefined
-            ability.title.drawState = 0
-            ability.timer = timer
-        }
-    
+            activeAbilities.name = abilityName
+            activeAbilities.title = new Title({text: `&6[&3&kd&6] &b&l${abilityName}&6 [&3&kd&6]`})
+            //timer = activeAbilities.timer
+    }
 
+    //if this is first check apply default attributes and create object
     if (!found)
     {
+        //print(object)
         let object = {timer, name: abilityName, title: new Title({text: `&6[&3&kd&6] &b&l${abilityName}&6 [&3&kd&6]`})}
         activeAbilities = (object)
         drawTimestamp = timer
         activeAbilities.title.drawState = 0
         activeAbilities.timer = timer
+        //print(activeAbilities.timer)
     }
+    
+    
 }
